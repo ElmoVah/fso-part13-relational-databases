c2ad02138c74ad04302dedab01c00fb8966e6e89
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-11-20 14:29:41 +0200

    Completed: Task 13.16.
    Make a route for the application /api/authors that returns the number of blogs for each author and the total number of likes. Implement the operation directly at the database level. You will most likely need the group by functionality, and the sequelize.fn aggregator function.
    
    Bonus task: order the data returned based on the number of likes, do the ordering in the database query.

---
+++ controllers/authors.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const { sequelize } = require('../util/db')
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    group: 'author',
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('title')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+    ],
+    order: [
+      [sequelize.fn("sum", sequelize.col("likes")), "DESC"]
+    ]
+  })
+  res.json(authors)
+})
+
+module.exports = router
